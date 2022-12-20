488e486037472d921f762f39ece0afda5eb56bff
Patryk Blaszczyk <patryk.blaszczyk@untitledkingdom.com>
2022-12-20 19:52:38 +0100

    solution-1

--- src/index.js
+++ src/index.js
@@ -6,11 +6,40 @@ const path = require("path");
   const mobileDevices = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/mobile_devices.json"), "utf-8"));
   const iotDevices = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/iot_devices.json"), "utf-8"));
 
-  console.log(new Date().toISOString());
+  const startTime = performance.now();
   console.log(count(users, mobileDevices, iotDevices));
-  console.log(new Date().toISOString());
+  const endTime = performance.now();
+  console.log(`funtion count took ${endTime - startTime} milliseconds`)
+
 })();
 
+
 function count(users, mobileDevices, iotDevices) {
-  // your code goes here
+return users.map((user) => {
+  // find all devices from a given user
+  const devices = mobileDevices.filter((device) => device.user === user.id)
+
+  if(!devices.length) {
+    return `${user.name} has no iot devices`
+  }
+
+  // find all iots from a given user
+  const iots = [];
+  devices.forEach((dev) => {
+  const filteredIotArr = iotDevices.filter((iot) => iot.mobile === dev.id)
+  const iterator = filteredIotArr.values();
+  for(const iot of iterator) {
+    iots.push(iot)
+  }
+})
+
+  return `${user.name} owns ${devices.length} mobile devices and ${iots.length} iots`
+  // return `${user.name} owns ${iots.length} iot devices in total`
+
+})
+
+
+
 }
+
+
