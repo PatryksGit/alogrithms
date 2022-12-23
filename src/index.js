eb5d413ca177c52bf04d072dd59d954415909806
Patryk Blaszczyk <patryk.blaszczyk@untitledkingdom.com>
2022-12-23 13:20:32 +0100

    solution-1 WIP

--- src/index.js
+++ src/index.js
@@ -2,44 +2,77 @@ const fs = require("fs");
 const path = require("path");
 
 (function init() {
-  const users = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/users.json"), "utf-8"));
-  const mobileDevices = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/mobile_devices.json"), "utf-8"));
-  const iotDevices = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../data/iot_devices.json"), "utf-8"));
+  const users = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, "../data/users.json"), "utf-8")
+  );
+  const mobileDevices = JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, "../data/mobile_devices.json"),
+      "utf-8"
+    )
+  );
+  const iotDevices = JSON.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, "../data/iot_devices.json"),
+      "utf-8"
+    )
+  );
 
   const startTime = performance.now();
   console.log(count(users, mobileDevices, iotDevices));
   const endTime = performance.now();
-  console.log(`funtion count took ${endTime - startTime} milliseconds`)
 
+  const duration = (endTime - startTime) / 1000;
+  console.log(`function count took ${duration.toFixed(2)} seconds`);
 })();
 
+// function count(users, mobileDevices, iotDevices) {
+//   const mobiles_to_users = {};
+//
+//   const users_with_iots = iotDevices.reduce((acc, iot) => {
+//     if (!mobiles_to_users[iot.mobile]) {
+//       mobiles_to_users[iot.mobile] = mobileDevices.find((currentMobile) => {
+//         return currentMobile.id === iot.mobile;
+//       }).user;
+//     }
+//     const userId = mobiles_to_users[iot.mobile];
+//
+//     if (!acc[userId]) {
+//       acc[userId] = [];
+//     }
+//     acc[userId].push(iot);
+//     return acc;
+//   }, {});
+//   console.log(users_with_iots);
+// }
 
 function count(users, mobileDevices, iotDevices) {
-return users.map((user) => {
-  // find all devices from a given user
-  const devices = mobileDevices.filter((device) => device.user === user.id)
+  let mobileDevicesCopy = [...mobileDevices];
 
-  if(!devices.length) {
-    return `${user.name} has no iot devices`
-  }
+  return users.map((user) => {
+    // find all devices from a given user
+    const devices = mobileDevicesCopy.filter(
+      (device) => device.user === user.id
+    );
 
-  // find all iots from a given user
-  const iots = [];
-  devices.forEach((dev) => {
-  const filteredIotArr = iotDevices.filter((iot) => iot.mobile === dev.id)
-  const iterator = filteredIotArr.values();
-  for(const iot of iterator) {
-    iots.push(iot)
-  }
-})
+    mobileDevicesCopy = mobileDevicesCopy.filter(
+      (mobile) => !devices.includes(mobile)
+    );
 
-  return `${user.name} owns ${devices.length} mobile devices and ${iots.length} iots`
-  // return `${user.name} owns ${iots.length} iot devices in total`
+    // find all iots from a given user
+    if (!devices.length) {
+      return `${user.name} has no iot devices`;
+    }
 
-})
+    const iots = [];
+    devices.forEach((dev) => {
+      const filteredIotsArr = iotDevices.filter((iot) => iot.mobile === dev.id);
+      const iterator = filteredIotsArr.values();
+      for (const iot of iterator) {
+        iots.push(iot);
+      }
+    });
 
-
-
+    return `${user.name} owns ${devices.length} mobile devices and ${iots.length} iots`;
+  });
 }
-
-
